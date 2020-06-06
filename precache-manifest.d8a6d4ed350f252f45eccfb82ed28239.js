5425c9f4a7b31b6ac739d8945018f37f50f56db0
PG-CureFit <prashant.gupta@curefit.com>
2020-06-06 18:51:52 +0530

    Updates

---
+++ precache-manifest.d8a6d4ed350f252f45eccfb82ed28239.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464285c806f31a5ce2d71ac97558d462",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "48d631dbb16bd52a4b31",
+    "url": "/home/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/home/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/home/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48d631dbb16bd52a4b31",
+    "url": "/home/static/js/main.8a688483.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "881f6662e2b965cf7317a50086aaf74e",
+    "url": "/home/static/media/resume.881f6662.pdf"
+  }
+]);
